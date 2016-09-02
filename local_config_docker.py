c6016b99a82b8ec313739bd694c35515a15e6208
Manuel Gómez <targen@gmail.com>
2016-09-02 17:55:24 -0400

    Dockerize application

---
+++ local_config_docker.py
@@ -0,0 +1,19 @@
+# Apparently unused options for django.contrib.sites:
+SITE_DOMAIN = ''
+SITE_HOST = ''
+HOST = 'localhost'
+
+# Disabled if running in debugging mode, so this is probably not important for now:
+ALLOWED_HOSTS = []
+
+DEBUG = True
+
+# Secrets:
+ADDRESS_API_KEY = ''
+
+# Database connection setup:
+DATABASE_HOST = 'sundog_db'
+DATABASE_PORT = '5432'
+DATABASE_NAME = 'sundog_db'
+DATABASE_USER = 'sundog_db'
+DATABASE_PASSWORD = 'sundog_db'
