e1784f036de8614211bb6f193940489a38f5d177
Manuel Gómez <targen@gmail.com>
2016-10-25 17:02:10 -0400

    Move fm_form script and style to static files

---
+++ sundog/static/js/sundog/fm_form.js
@@ -0,0 +1,39 @@
+/* Fix bootstrap-select and TinyMCE instances inside the modal form */
+function tinymce_setup(editor) {
+  editor.on(
+    'change',
+    function () {
+      editor.save();
+    }
+  );
+}
+
+function initialize_modal_contents() {
+  $('.selectpicker').selectpicker('render');
+
+  tinyMCE.remove('.tinymce');
+  $('.tinymce').each(
+    function() {
+      initTinyMCE($(this));
+    }
+  );
+  return false;
+}
+
+$(
+  function() {
+    $('#fm-modal').on(
+      'fm.ready',
+      initialize_modal_contents
+    );
+  }
+);
+
+/*
+The selectpicker controls need to be initialized every time the FM modal
+dialog loads.  This can be mostly solved with a handler for the `fm.ready`
+event, but the first time the FM modal dialog loads, this script has not yet
+registered the event handler.  Therefore, the initialization function needs
+to be executed manually for the first initialization.
+*/
+initialize_modal_contents();
